a838b72ab6eab9de05ca7dc06deb16f72492761f
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-05-21 19:35:29 +0300

    Initial content

---
+++ README.md
@@ -0,0 +1,5 @@
+# PyRPG
+
+This is a small library that facilitates creating Role-Playing Games in Python. It is designed with classic dice-based RPG systems in mind.
+
+## Example usage
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyrpg",
+    version="0.0.1",
+    author="feakuru",
+    author_email="feanarokurufinve@gmail.com",
+    description="A library for writing RPGs in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/feakuru/pyrpg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
