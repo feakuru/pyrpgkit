7a603883cf0b1380ec27fc2332ea7f7b6961c9d1
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-05-21 20:55:25 +0300

    Created initial classes for dialogs

---
+++ pyrpgkit/dialog.py
@@ -0,0 +1,16 @@
+"""Classes describing entities pertaining to dialogs."""
+
+
+class Line:
+    """A single line of a dialog."""
+    pass
+
+
+class DialogHistory:
+    """A chronological sequence of Lines."""
+    pass
+
+
+class DialogTree:
+    """A tree of Lines representing all possible ways a conversation can go."""
+    pass
