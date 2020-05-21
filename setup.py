97e79cd8db98a874bb10c2dc199d121b8e3185e5
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-05-21 19:45:45 +0300

    Renamed package to avoid colliding names

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyrpg",
+    name="pyrpgkit",
     version="0.0.1",
     author="feakuru",
     author_email="feanarokurufinve@gmail.com",
     description="A library for writing RPGs in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/feakuru/pyrpg",
+    url="https://github.com/feakuru/pyrpgkit",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
