68db991916956f8a1c22122bc35a124f6ec1d70a
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-05-21 20:54:52 +0300

    Created initial specialty classes

---
+++ pyrpgkit/specialty.py
@@ -0,0 +1,9 @@
+"""Classes describing character classes and specialties."""
+
+class CharacterClass:
+    """A class of a character, e.g. a wizard or a warrior."""
+    pass
+
+class CharacterSpecialty:
+    """A certain specialty not treated as a main class, e.g. a blacksmith."""
+    pass
