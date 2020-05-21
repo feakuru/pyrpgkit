96d3ca1d3841fab36d21ce47358d7896ea8c5eb6
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-05-21 20:55:11 +0300

    Created initial turn classes

---
+++ pyrpgkit/turn.py
@@ -0,0 +1,15 @@
+"""Classes describing turns and other chronological entities."""
+
+class Move:
+    """A single move, e.g. an attemt to use an Ability."""
+    pass
+
+
+class Turn:
+    """A time-ordered series of Actions."""
+    pass
+
+
+class TurnHistory:
+    """A chronological sequence of Turns."""
+    pass
