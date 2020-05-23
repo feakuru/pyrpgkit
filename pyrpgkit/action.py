3a80d7a7d1aea2b08eb18c0d2e00358f8ba2f26e
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-05-24 00:32:32 +0300

    Action now behaves according to tests

--- pyrpgkit/action.py
+++ pyrpgkit/action.py
@@ -2,7 +2,15 @@
 
 class Action:
     """Base class for all actions."""
-    pass
+
+    def __init__(self, action_type, action_summary):
+        """
+        Args:
+            action_type: The type of the action in the logic of your game.
+            action_summary: A short human-readable description of the action.
+        """
+        self.action_type = action_type
+        self.action_summary = action_summary
 
 
 class Ability(Action):
