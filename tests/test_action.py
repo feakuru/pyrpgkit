8bfda4efd1a566783d7cb268d70e732a97c0d01f
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-05-24 00:35:37 +0300

    Testing Ability and agnostic summary param name

--- tests/test_action.py
+++ tests/test_action.py
@@ -14,7 +14,21 @@ class TestAction:
         summary = "move one step forward"
         act = action.Action(
             action_type=act_type,
-            action_summary=summary
+            summary=summary
         )
         assert act.action_type == act_type
-        assert act.action_summary == summary
+        assert act.summary == summary
+
+
+class TestAbility:
+    def test_can_not_create_without_parameters(self):
+        with pytest.raises(TypeError):
+            abl = action.Ability()
+
+    def test_can_create_with_all_parameters(self):
+        summary = "summon Grokus"
+        abl = action.Ability(
+            summary=summary
+        )
+        assert act.action_type == "ability"
+        assert act.summary == summary
