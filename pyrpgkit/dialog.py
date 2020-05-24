7a0ee2aed5af5fbda7008382a14fc9e36e636e8c
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-05-24 23:54:17 +0300

    Implemented basic dialog behaviour

--- pyrpgkit/dialog.py
+++ pyrpgkit/dialog.py
@@ -1,14 +1,34 @@
 """Classes describing entities pertaining to dialogs."""
 
+from typing import Union
+from .actor import Character
+from queue import Queue
 
 class Line:
     """A single line of a dialog."""
-    pass
+    def __init__(self, text: str, author: Union[Character, str] = None):
+        self.text = text
+        self.author = author
+    
+    def __str__(self):
+        if self.author is None:
+            return self.text
+        return "{}: \"{}\"".format(self.author, self.text)
 
 
 class DialogHistory:
     """A chronological sequence of Lines."""
-    pass
+    def __init__(self):
+        self._lines = []
+    
+    def add(self, line: Line):
+        if not isinstance(line, Line):
+            raise TypeError("line must be an instance of Line")
+        self._lines.append(line)
+    
+    @property
+    def history(self):
+        yield from self._lines
 
 
 class DialogTree:
